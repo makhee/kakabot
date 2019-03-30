b6ce4fd58b816473cafd2bc9841a6e1a091e77a2
MAKHEE <makhee@megabrain.co.kr>
2019-03-30 15:02:14 +0900

    날씨 테스트.

---
+++ app/Http/Controllers/Weather.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Goutte\Client;
+
+class Weather extends Controller
+{
+
+    public function getWeather($city)
+    {
+        $url = 'https://search.naver.com/search.naver?query=날씨+동대문';
+
+        $client = new Client();
+    
+        $crawler = $client->request('GET', $url);
+        $crawler->filter('.info_data')->each(function ($node) {
+            echo $node->text()."\n";
+        });
+        
+    }
+    
+}
